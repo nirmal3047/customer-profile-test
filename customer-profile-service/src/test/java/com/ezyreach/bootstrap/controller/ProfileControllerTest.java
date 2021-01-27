e79a3339921f3879a226e31ed94e32c7fb39bbac
Nirmal Kumar <nirmal3047@gmail.com>
2021-01-27 08:50:27 +0530

    test modified

--- customer-profile-service/src/test/java/com/ezyreach/bootstrap/controller/ProfileControllerTest.java
+++ customer-profile-service/src/test/java/com/ezyreach/bootstrap/controller/ProfileControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -25,11 +25,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 public class ProfileControllerTest {
 
-	private MockMvc mockMvc;
+	private static MockMvc mockMvc;
 	
-	@Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProfileController()).build();
+	@BeforeClass
+    public static void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new ProfileController()).build();
     }
 
 	@Test
