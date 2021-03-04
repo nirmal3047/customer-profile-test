fa83019ea910edaf3afa7a6d3805020f63f1f40c
Nirmal Kumar <nirmal3047@gmail.com>
2021-03-04 10:14:53 +0530

    dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM openjdk:8 AS build
+ADD . /src
+WORKDIR /src
+ARG COMMIT_ID
+ENV COMMIT_ID=$COMMIT_ID
+RUN ./gradlew --no-daemon service:clean service:build service:bootJar  --info
+FROM openjdk:8-jre
+EXPOSE 9288
+# HEALTHCHECK --retries=12 --interval=90s CMD curl -s localhost:9290/health || exit 1
+COPY --from=build /src/service/build/libs/service-*.jar /usr/local/bin/service.jar
+COPY --from=build /src/run.sh /usr/local/bin/run.sh
+RUN chmod +x /usr/local/bin/service.jar
+RUN chmod +x /usr/local/bin/run.sh
+
+CMD ["/usr/local/bin/run.sh"]
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Based on a template by BASH3 Boilerplate v2.3.0
+# http://bash3boilerplate.sh/#authors
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use dollar{VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar /usr/local/bin/service.jar
